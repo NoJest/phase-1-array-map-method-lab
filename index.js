c5444bb9a961d08860ae8dafde954dc561fbb498
JustinpThomasson <Foreveraendeavour@gmail.com>
2024-10-14 16:02:10 -0400

    just forced

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ const tutorials = [
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
   'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
+  'What is the difference between StopPropagation and PreventDefault?',
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
 
+// Here when i got here
+
 const titleCased = () => {
-  return tutorials
-}
+  const ignore = ['OO', "API", 'NaN', 'JSONP?', 'StopPropagation', 'PreventDefault?' ]
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      if (ignore.includes(word)) {
+        return word;
+      }else {
+      return word.charAt(0).toUpperCase()+word.slice(1).toLowerCase();
+      }
+    }).join(' ');
+  });
+};
+
+
+
